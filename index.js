49156e2a10a628cf6fe3186d94a4c7ed64c403a4
Sayed Mohammad Jonayet <108022854+sm-jonayet-143@users.noreply.github.com>
2022-11-08 23:22:59 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+// console.log('connected');
+function firstButton(){
+    document.getElementById('myImage').src='images/images.jpg';
+}
+function secoendButton(){
+    document.getElementById('myImage').src='images/pic_bulboff.gif';
+}
